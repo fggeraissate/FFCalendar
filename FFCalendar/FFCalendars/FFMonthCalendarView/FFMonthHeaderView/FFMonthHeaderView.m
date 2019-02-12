cb76c1c3dc61d420963ed80b7d2320c194cd3a64
Mohsinali Matiya <mmatiyailol@gmail.com>
2019-02-12 11:27:12 +0530

    Update FFMonthHeaderView.m
    set month label in center.

--- FFCalendar/FFCalendars/FFMonthCalendarView/FFMonthHeaderView/FFMonthHeaderView.m
+++ FFCalendar/FFCalendars/FFMonthCalendarView/FFMonthHeaderView/FFMonthHeaderView.m
@@ -26,7 +26,7 @@
         for (int i = 0; i < [arrayWeekAbrev count]; i++) {
 
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(i*(width+SPACE_COLLECTIONVIEW_CELL), 0., width-5., self.frame.size.height)];
-            [label setTextAlignment:NSTextAlignmentRight];
+            [label setTextAlignment:NSTextAlignmentCenter];
             [label setText:[arrayWeekAbrev objectAtIndex:i]];
             [label setTextColor:[arrayColor objectAtIndex:i]];
             [label setFont:[UIFont boldSystemFontOfSize:label.font.pointSize]];
